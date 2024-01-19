3397245f2d6ced8f55c5c1178613bb5723cd4317
서울_17반_이현진 <hgene0929@gmail.com>
2024-01-19 20:34:16 +0900

    feat: Add First Commit

---
+++ src/main/java/com/example/testinfra/TestinfraApplication.java
@@ -0,0 +1,13 @@
+package com.example.testinfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestinfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestinfraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testinfra/TestinfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestinfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
