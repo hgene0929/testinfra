87959e062b57155a34b64e7ce0cc3c59d734d081
서울_17반_이현진 <hgene0929@gmail.com>
2024-01-20 08:21:15 +0900

    feat: Add Test Code Commit

--- src/main/java/com/example/testinfra/TestApi.java
+++ src/main/java/com/example/testinfra/TestApi.java
@@ -3,11 +3,23 @@ package com.example.testinfra;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class TestApi {
 
+	private final TestEntityRepository testEntityRepository;
+
 	@GetMapping("/")
 	public String test() {
-		return "[spring boot - jenkins cicd] SUCCEEDED!!";
+		int idx = testEntityRepository.findAll().size();
+
+		TestEntity testEntity = new TestEntity();
+		testEntity.setName("ENTITY " + idx+1);
+
+		testEntityRepository.save(testEntity);
+
+		return "BUILD SUCCEEDED!!";
 	}
 }
